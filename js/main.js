2cfa036d8b5f9e8b42b1fdae3bbe6c1f2b233b81
Camia Trefs Sebastian <logicalbrainstudio@gmail.com>
2023-04-14 19:53:07 -0300

    JS tweak

--- js/main.js
+++ js/main.js
@@ -1,44 +1,58 @@
 // Function to handle scroll animations
 function handleScrollAnimation(elementId, fadeInClass, fadeOutClass, fadePercentage) {
-    var scrollTop = $(window).scrollTop();
-    var windowHeight = $(window).height();
-    var fadeElement = $(elementId);
-    var fadeOutPosition = scrollTop + (windowHeight * (1 - fadePercentage));
-    var fadeInPosition = scrollTop + (windowHeight * fadePercentage);
+    const scrollTop = $(window).scrollTop();
+    const windowHeight = $(window).height();
+    const fadeElement = $(elementId)[0];
+    const fadeOutPosition = scrollTop + (windowHeight * (1 - fadePercentage));
+    const fadeInPosition = scrollTop + (windowHeight * fadePercentage);
 
-    if (fadeElement.offset().top + fadeElement.outerHeight() > fadeInPosition && fadeElement.offset().top < fadeOutPosition) {
-        fadeElement.addClass(fadeInClass);
-        fadeElement.removeClass(fadeOutClass);
+    if (fadeElement.offsetTop + fadeElement.offsetHeight > fadeInPosition && fadeElement.offsetTop < fadeOutPosition) {
+        fadeElement.classList.add(fadeInClass);
+        fadeElement.classList.remove(fadeOutClass);
     } else {
-        fadeElement.removeClass(fadeInClass);
-        fadeElement.addClass(fadeOutClass);
+        fadeElement.classList.remove(fadeInClass);
+        fadeElement.classList.add(fadeOutClass);
     }
 }
 
+// Debounce function to limit the rate of function calls
+function debounce(func, wait) {
+    let timeout;
+    return function () {
+        clearTimeout(timeout);
+        timeout = setTimeout(func, wait);
+    };
+}
+
 // CODE FOR SECTION HOME
-$(window).scroll(function () {
-    handleScrollAnimation('#fadeLeft', 'animate__fadeInLeft', 'animate__fadeOutLeft', 0.5);
-    handleScrollAnimation('#fadeRight', 'animate__fadeInRight', 'animate__fadeOutRight', 0.5);
-});
+const handleScrollHome = debounce(function () {
+    handleScrollAnimation('#fadeLeft', 'animate__fadeInLeft', 'animate__fadeOutLeft', 0.3);
+    handleScrollAnimation('#fadeRight', 'animate__fadeInRight', 'animate__fadeOutRight', 0.3);
+}, 100);
 
 // CODE FOR SECTION ABOUT ME
-$(window).scroll(function () {
-    handleScrollAnimation('#backLeft', 'animate__backInLeft', 'animate__backOutLeft', 0.5);
-    handleScrollAnimation('#backRight', 'animate__backInRight', 'animate__backOutRight', 0.5);
-});
+const handleScrollAboutMe = debounce(function () {
+    handleScrollAnimation('#backLeft', 'animate__backInLeft', 'animate__backOutLeft', 0.3);
+    handleScrollAnimation('#backRight', 'animate__backInRight', 'animate__backOutRight', 0.3);
+}, 100);
 
 // CODE FOR SECTION PORTFOLIO
-$(window).scroll(function () {
-    handleScrollAnimation('#fadeInTopLeft', 'animate__fadeInTopLeft', 'animate__fadeOutTopLeft', 0.5);
-    handleScrollAnimation('#fadeInTopRight', 'animate__fadeInTopRight', 'animate__fadeOutTopRight', 0.5);
-});
+const handleScrollPortfolio = debounce(function () {
+    handleScrollAnimation('#fadeInTopLeft', 'animate__fadeInTopLeft', 'animate__fadeOutTopLeft', 0.3);
+    handleScrollAnimation('#fadeInTopRight', 'animate__fadeInTopRight', 'animate__fadeOutTopRight', 0.3);
+}, 100);
 
 // CODE FOR SECTION CONTACT
-$(window).scroll(function () {
-    handleScrollAnimation('#bounceIn', 'animate__bounceIn', 'animate__bounceOut', 0.5);
-    handleScrollAnimation('#bounceIn2', 'animate__bounceIn', 'animate__bounceOut', 0.5);
-});
+const handleScrollContact = debounce(function () {
+    handleScrollAnimation('#bounceIn', 'animate__bounceIn', 'animate__bounceOut', 0.3);
+    handleScrollAnimation('#bounceIn2', 'animate__bounceIn', 'animate__bounceOut', 0.3);
+}, 100);
 
+// Bind debounced scroll events to window
+$(window).scroll(handleScrollHome);
+$(window).scroll(handleScrollAboutMe);
+$(window).scroll(handleScrollPortfolio);
+$(window).scroll(handleScrollContact);
 
 // INIT BOOTSTRAP TOOLTIPS
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
