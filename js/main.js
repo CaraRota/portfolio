e6089a07b0cfa24703ffebaf7fb47e91f57b2c47
Camia Trefs Sebastian <logicalbrainstudio@gmail.com>
2023-04-14 19:30:48 -0300

    quickfix JS

--- js/main.js
+++ js/main.js
@@ -17,26 +17,26 @@ function handleScrollAnimation(elementId, fadeInClass, fadeOutClass, fadePercent
 
 // CODE FOR SECTION HOME
 $(window).scroll(function () {
-    handleScrollAnimation('#fadeLeft', 'animate__fadeInLeft', 'animate__fadeOutLeft', 0.5);
-    handleScrollAnimation('#fadeRight', 'animate__fadeInRight', 'animate__fadeOutRight', 1);
+    handleScrollAnimation('#fadeLeft', 'animate__fadeInLeft', 'animate__fadeOutLeft', 0.3);
+    handleScrollAnimation('#fadeRight', 'animate__fadeInRight', 'animate__fadeOutRight', 0.3);
 });
 
 // CODE FOR SECTION ABOUT ME
 $(window).scroll(function () {
-    handleScrollAnimation('#backLeft', 'animate__backInLeft', 'animate__backOutLeft', 0.5);
-    handleScrollAnimation('#backRight', 'animate__backInRight', 'animate__backOutRight', 1);
+    handleScrollAnimation('#backLeft', 'animate__backInLeft', 'animate__backOutLeft', 0.3);
+    handleScrollAnimation('#backRight', 'animate__backInRight', 'animate__backOutRight', 0.3);
 });
 
 // CODE FOR SECTION PORTFOLIO
 $(window).scroll(function () {
-    handleScrollAnimation('#fadeInTopLeft', 'animate__fadeInTopLeft', 'animate__fadeOutTopLeft', 0.5);
-    handleScrollAnimation('#fadeInTopRight', 'animate__fadeInTopRight', 'animate__fadeOutTopRight', 1);
+    handleScrollAnimation('#fadeInTopLeft', 'animate__fadeInTopLeft', 'animate__fadeOutTopLeft', 0.3);
+    handleScrollAnimation('#fadeInTopRight', 'animate__fadeInTopRight', 'animate__fadeOutTopRight', 0.3);
 });
 
 // CODE FOR SECTION CONTACT
 $(window).scroll(function () {
-    handleScrollAnimation('#bounceIn', 'animate__bounceIn', 'animate__bounceOut', 0.5);
-    handleScrollAnimation('#bounceIn2', 'animate__bounceIn', 'animate__bounceOut', 1);
+    handleScrollAnimation('#bounceIn', 'animate__bounceIn', 'animate__bounceOut', 0.3);
+    handleScrollAnimation('#bounceIn2', 'animate__bounceIn', 'animate__bounceOut', 0.3);
 });
 
 
