e26de859f359888a45b47107b729428b352845be
Camia Trefs Sebastian <logicalbrainstudio@gmail.com>
2023-04-19 17:03:10 -0300

    Toastify and Form introduction

--- js/main.js
+++ js/main.js
@@ -4,3 +4,34 @@ AOS.init();
 // INIT BOOTSTRAP TOOLTIPS
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+// SEND BUTTON STARTS HERE
+const sendMsg = (e) => {
+    e.preventDefault()
+    const inputName = document.querySelector("#inputName")
+    const name = inputName.value || alert("Please verify your Name")
+}
+
+const btnSend = document.querySelector("#btnSend")
+btnSend.addEventListener("submit", sendMsg)
+
+
+// RESET BUTTON STARTS HERE
+const btnReset = document.querySelector("#btnReset")
+btnReset.addEventListener("click", resetForm = () => {
+    if (inputName.value.length > 0 || inputEmail.value.length > 0 || inputMessage.value.length > 0) {
+        const form = document.querySelector("#form")
+        form.reset()
+        Toastify({
+            text: "Form has been cleaned up!",
+            gravity: "top",
+            position: "right",
+        }).showToast();
+    } else {
+        Toastify({
+            text: "You should try writing something first!",
+            gravity: "top",
+            position: "right",
+        }).showToast();
+    }
+})
